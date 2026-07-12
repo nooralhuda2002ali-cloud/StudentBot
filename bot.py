3701dfaf15dcfcec3b775b7f09ec18857528f994
nooralhuda2002ali-cloud <nooralhuda2002.ali@gmail.com>
2026-07-12 14:40:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,3 +63,60 @@ async def send_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
             caption=info +
             f"\n📄 ملف\n\nللرد:\n/reply {user.id} الرسالة"
         )
+        # فيديو
+async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    user = update.effective_user
+
+    await context.bot.send_video(
+        chat_id=ADMIN_ID,
+        video=update.message.video.file_id,
+        caption=(
+            f"🎥 فيديو جديد\n\n"
+            f"👤 الاسم: {user.full_name}\n"
+            f"🆔 ID: {user.id}\n\n"
+            f"للرد:\n/reply {user.id} الرسالة"
+        )
+    )
+
+
+# صوت / تسجيل
+async def handle_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    user = update.effective_user
+
+    await context.bot.send_audio(
+        chat_id=ADMIN_ID,
+        audio=update.message.audio.file_id,
+        caption=(
+            f"🎵 ملف صوتي جديد\n\n"
+            f"👤 الاسم: {user.full_name}\n"
+            f"🆔 ID: {user.id}\n\n"
+            f"للرد:\n/reply {user.id} الرسالة"
+        )
+    )
+
+
+# الرد على الطالب
+async def reply_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    try:
+        user_id = int(context.args[0])
+        text = " ".join(context.args[1:])
+
+        await context.bot.send_message(
+            chat_id=user_id,
+            text=text
+        )
+
+        await update.message.reply_text(
+            "✅ تم إرسال الرد"
+        )
+
+    except:
+        await update.message.reply_text(
+            "استخدم:\n/reply ID الرسالة"
+        )
